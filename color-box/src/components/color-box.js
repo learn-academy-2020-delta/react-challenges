dfb114fb053ae1f7a71e0ff1abab58288bdf8e58
crgjr66 <chuckgilbreth@yahoo.com>
2020-09-14 16:04:49 -0700

    got color box we're almost there

---
+++ color-box/src/components/color-box.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+class ColorBox extends Component {
+  constructor(props){
+    super(props)
+      this.state = {
+        colorOption : ["green", "blue", "yellow", "red", "purple", "orange"],
+        // bkGroundColor : ["green", "blue", "yellow", "red", "purple", "orange"],
+        currentColor: "green"
+    }
+  }
+
+  handleChange = () => {
+    let { colorOption, bkGroundColor, currentColor } = this.state
+    let colorIndex = Math.floor(Math.random() * 6)
+    let colorValue = colorOption[colorIndex]
+    // console.log(colorIndex)
+    // let bkGroundColorValue = bkGroundColor[colorIndex]
+    this.setState({currentColor: colorValue})
+  }
+
+  render() {
+    const headerStyle = {
+      color: "black",
+      fontFamily: "Arial"
+    }
+    return (
+      <div>
+        <h2 style={{ headerStyle }}>Ryan & Chuck's Color Box</h2>
+        <div id="whiteSquare" style={{background: this.state.currentColor}}></div>
+        <button onClick = {this.handleChange}> Click Me
+        </button>
+        <h3>{this.state.currentColor}</h3>
+      </div>
+    )
+  }
+}
+
+export default ColorBox
