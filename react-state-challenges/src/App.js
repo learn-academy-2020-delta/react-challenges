d6edbd2185cc636aae534d6d8d59303bd520729b
bnddiep <bach.nhu.diep@gmail.com>
2020-09-14 15:14:00 -0700

    Initial commit.

---
+++ react-state-challenges/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Boxes from './components/Boxes.js'
+import './App.css';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <Boxes />
+      </div>
+    );
+  }
+}
+
+export default App;
