9c152bbf22e2200e4179f37d9adc74f619fa1611
mmullis07 <eddiemull311@gmail.com>
2020-09-14 16:46:02 -0700

    started lightbulb app

---
+++ light-bulb-challenge-brian-matt/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+import LightSwitch from "./components/Lightswitch";
+
+export default class App extends Component {
+  return (
+    
+  )
+}
+
---
+++ light-bulb-challenge-brian-matt/src/components/Lightswitch.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+export default class LightSwitch extends Component {
+    constructor(props) {
+        super(props) 
+        this.state = {
+            // colorOptions: ["green", "blue", "yellow", "red", "purple", "orange"],
+            switchPosition: "off",
+            backgroundColor: "white"
+        }
+    }
+    handleChange = () => {
+        let {colorOptions} = this.state
+        let chosenColor = colorOptions[Math.floor(Math.random()* 6)] 
+        this.setState({colorText: chosenColor, backgroundColor: chosenColor})
+        
+    }
+    render() {
+        return(
+            <div className="square" onClick={this.handleChange} style={{backgroundColor: this.state.backgroundColor}}>
+                {this.state.colorText}
+            </div>
+        )
+    }
+}
