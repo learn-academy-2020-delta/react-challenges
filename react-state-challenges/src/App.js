145017651201c0ca6a07bef1f4a7cdf7810d4560
NateDawg360 <nathan_loranca@yahoo.com>
2020-09-14 16:44:20 -0700

    button works now

--- react-state-challenges/src/App.js
+++ react-state-challenges/src/App.js
@@ -7,6 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <Boxes />
+        <Boxes />
+        <Boxes />
       </div>
     );
   }
--- react-state-challenges/src/components/Boxes.js
+++ react-state-challenges/src/components/Boxes.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 
 class Boxes extends Component {
     constructor(props) {
-        super(props) 
+        super(props)
             this.state = {
                 //   index:      0       1        2         3       4       5         6
                 squareColor: ["white", "green", "blue", "yellow", "red", "purple", "orange"],
                 // nameOfColor is associated with the button name
-                nameOfColor: ["white", "green", "blue", "yellow", "red", "purple", "orange"]
+                nameOfColor: "white"
         }
     }
     //handle change method that will change the name of button and its corresponding color*/}
@@ -16,19 +16,25 @@ class Boxes extends Component {
         let { squareColor, nameOfColor } = this.state
         // create variable called randomColor that holds the random number associated with that color in array
         let randomColor = squareColor[Math.floor(Math.random() * 6)]
-        // create a single random number corresponds with the color of square
-        
         // want to make sure that random square color === nameOfColor
-
+        this.setState({nameOfColor: randomColor})
         // randomColor has to equal the string name of the color
     }
 
     render() {
+      const buttonStyle = {
+        color: "black",
+        backgroundColor: this.state.nameOfColor,
+        fontFamily: "Arial",
+        border: "none",
+        borderColor: "gray",
+        padding: "50px 50px",
+      }
         return(
             <>
                 <h1>COLOR SQUARE</h1>
                 {/* use template literals to change button name to nameOfColor */}
-                <button onClick = {this.handleChange}>
+                <button style={buttonStyle} onClick = {this.handleChange}>
                     { this.state.nameOfColor }
                 </button>
             </>
