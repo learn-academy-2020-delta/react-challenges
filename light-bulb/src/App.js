f9d9f1bcca4c2c9eb9472ff4b62ace2125258c93
davidkim4 <dkim4016@gmail.com>
2020-09-14 18:03:36 -0700

    removed boilerplate for lightbulb

--- light-bulb/src/App.js
+++ light-bulb/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
+import Bulb from '. /components/Bulb.js';
+import './App.css'
 
 class App extends Component {
   render() {
---
+++ light-bulb/src/components/Bulb.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class Bulb extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+
+
+
+
+
+
+
+
+
+
+    render() {
+        return (
+
+
+
+
+
+        )
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+export default Bulb
