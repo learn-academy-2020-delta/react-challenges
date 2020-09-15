e3b7bba93497de20808e84666076cd12933055cc
drewbrad4 <drewbrad4@gmail.com>
2020-09-15 11:09:09 -0700

    challenges finished, started styling

--- diceroller-drew-matt/src/App.js
+++ diceroller-drew-matt/src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Dice from './Components/Dice';
-//import Rolls from './Components/Rolls';
+import Rolls from './Components/Rolls';
 
 
 class App extends Component {
@@ -15,17 +15,27 @@ constructor(props){
   }
 
 randomNumber = () => {
+  let { rollHistory, rolledNumber, diceRolls} = this.state
   let number = Math.floor(Math.random()* this.state.diceRolls.length)
-  this.setState({rolledNumber: number})
+  this.setState({rolledNumber: diceRolls[number]})
+  rollHistory.push(diceRolls[number])
+  console.log(this.state.rollHistory)
 }
 
   render() {
     return (
       <div>
-        <Dice 
-          roller={this.randomNumber}
-          number={this.state.rolledNumber}
-        />
+        <div>
+          <Dice 
+            roller={this.randomNumber}
+            number={this.state.rolledNumber}
+          />
+        </div>
+        <div>
+          <Rolls 
+            rolls={this.state.rollHistory}
+          />
+        </div>
       </div>
     )
   }
--- diceroller-drew-matt/src/Components/Rolls.js
+++ diceroller-drew-matt/src/Components/Rolls.js
@@ -3,8 +3,17 @@ import React, {Component} from 'react';
 class Rolls extends Component {
 
   render() {
+    let eachRoll = this.props.rolls.map(roll => {
+        return(
+          <li>{roll}</li>
+        )
+    })
     return (
       <div>
+        <h3>Rolls:</h3>
+        <ul>
+          {eachRoll}
+        </ul>
       </div>
     )
   }
