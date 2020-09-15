d45b6df4c3f1da242825e75a27b66c28d78bbcf9
davidkim4 <dkim4016@gmail.com>
2020-09-14 22:37:28 -0700

    finished first half of lightbulb

--- light-bulb/src/App.js
+++ light-bulb/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Bulb from '. /components/Bulb.js';
+import Bulb from './components/Bulb.js';
 import './App.css'
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
 
     return (
       <div>
-
+        <Bulb />
       </div>
     );
   }
--- light-bulb/src/components/Bulb.js
+++ light-bulb/src/components/Bulb.js
@@ -3,42 +3,50 @@ import React, { Component } from 'react';
 class Bulb extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            color: "",
+            light: "off"
+
+        }
     }
 
 
 
-
-
-
-
-
-
+    handleChange = () => {
+        //destructure
+        let { light } = this.state
+        //ternary to switch between on and off.
+        return light === "off" ? this.setState({
+            color: "yellow",
+            light: "on",
+        }) : this.setState({
+            color: "white",
+            light: "off",
+        })
+    }
 
 
     render() {
+        const divStyle = {
+            margin: "auto",
+            width: "20%"
+        }
+
+        const buttonStyle = {
+            backgroundColor: this.state.color,
+            border: "1px, solid, #000",
+            fontSize: "50px",
+            height: "400px",
+            width: "400px",
+            padding: "30px 30px",
+
+        }
         return (
-
-
-
-
-
+            <div style={divStyle}>
+                <button style={buttonStyle} onClick={this.handleChange} > {this.state.light} </button>
+            </div >
         )
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 export default Bulb
