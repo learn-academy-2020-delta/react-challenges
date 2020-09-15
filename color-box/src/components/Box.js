fbd04f48f0973581dc19d92dad60f3a8d7819186
davidkim4 <dkim4016@gmail.com>
2020-09-14 17:57:30 -0700

    color box done and created lightbulb

--- color-box/src/components/Box.js
+++ color-box/src/components/Box.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
 
 class Box extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-            colorOptions: [ "red", "blue", "green", "orange", "yellow", "purple" ],
-            colorBackground: [ "red", "blue", "green", "orange", "yellow", "purple" ],
-            currentColor:""
-            //current color: "blank, white"
+            colorOptions: ["red", "blue", "green", "orange", "yellow", "purple"],
+            currentColor: "Click Me to Begin"
         }
     }
 
-    handleChange = () =>{
+    handleChange = () => {
         //destructure
         let { colorOptions } = this.state
         //create a var to hold the random color
         let newColor = colorOptions[Math.floor(Math.random() * 6)]
         // this.setState({ currentColor : newColor})
-        this.setState({ currentColor : newColor, colorBackground :  newColor })
+        this.setState({ currentColor: newColor })
     }
-        
+
     render() {
         const buttonStyle = {
-            backgroundColor: "white",
+            backgroundColor: this.state.currentColor,
             border: "1px, solid, #000",
             fontFamily: "papyrus",
             fontSize: "50px",
@@ -33,12 +31,9 @@ class Box extends Component {
         }
 
         return (
-                //see the white Box
-                // attach an onclick to box
-                //onClick = 
-                <div> 
-                    <button style = { buttonStyle } onClick = { this.handleChange }> { this.state.currentColor } </button>
-                </div>
+            <div>
+                <button style={buttonStyle} onClick={this.handleChange} > {this.state.currentColor}  </button>
+            </div>
         )
     }
 }
