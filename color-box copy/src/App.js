71cfcf90a1319002a9d5b8c1f51e92ac1cc408b6
davidkim4 <dkim4016@gmail.com>
2020-09-15 21:18:03 -0700

    fixing the file structure of all my previous branches

---
+++ color-box copy/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Box from './components/Box.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Box />
+        <Box />
+        <Box />
+      </div>
+    );
+  }
+}
+
+export default App;
